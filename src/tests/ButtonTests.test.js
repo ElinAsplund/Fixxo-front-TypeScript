23afff4d8a7b6018cd0ea7b7ed3bbee704f9d9fe
Elin Asplund <elin.asplund91@gmail.com>
2022-12-14 22:57:55 +0100

    restructuring of the tests, added comments

--- src/tests/ButtonTests.test.js
+++ src/tests/ButtonTests.test.js
@@ -3,6 +3,19 @@ import ProductOverview from '../sections/ProductOverview'
 import ShowcaseBottom from '../sections/ShowcaseBottom'
 
 describe( 'Button Tests' , () => {
+ 
+// ----Testing a button at ShowcaseBottom-component:----
+    
+    it('tests if button contains the correct text', () => {
+        const { getByTestId } = render(<ShowcaseBottom />)
+        
+        const shopNow = getByTestId('test-shop-now').textContent
+
+        expect(shopNow).toEqual('SHOP NOW')
+    })
+
+// ----Testing buttons at ProductOverview-component:----
+    // In order to make the tests run, a "fakeProduct" needed to be added as a prop of the ProductOverview-component:
     const fakeProduct ={
         id: "",
         tag: "",
@@ -11,24 +24,6 @@ describe( 'Button Tests' , () => {
         price: 0,
         imageName: ""
     }
-
-    it('Tests if button contains the correct text', () => {
-        const { getByTestId } = render(<ShowcaseBottom />)
-        
-        const shopNow = getByTestId('test-shop-now').textContent
-
-        expect(shopNow).toEqual('SHOP NOW')
-    })
-
-    it('tests if the class "size-button-dark" is added when the S size button is clicked', () => {
-        const { getByRole, getByTestId } = render(<ProductOverview productInfo={fakeProduct} />)
-        const sButton = getByRole('button', {name: 'S'})
-        
-        fireEvent.click(sButton)
-        const classNameS = getByTestId('sSize').className
-
-        expect(classNameS).toEqual('size-button me-2 size-button-dark')
-    })
 
     it('tests if size buttons contains the corresponding text: S, M, L, XL', () => {
         const { getByTestId } = render(<ProductOverview productInfo={fakeProduct} />)
@@ -42,6 +37,16 @@ describe( 'Button Tests' , () => {
         expect(valueM).toEqual('M')
         expect(valueL).toEqual('L')
         expect(valueXL).toEqual('XL')
+    })
+
+    it('tests if the class "size-button-dark" is added when the S size button is clicked', () => {
+        const { getByRole, getByTestId } = render(<ProductOverview productInfo={fakeProduct} />)
+        const sButton = getByRole('button', {name: 'S'})
+        
+        fireEvent.click(sButton)
+        const classNameS = getByTestId('sSize').className
+
+        expect(classNameS).toEqual('size-button me-2 size-button-dark')
     })
 
     it(`test if all other size button's classes doesn't contain the "size-button-dark" class, when S size buttons is clicked `, () => {
